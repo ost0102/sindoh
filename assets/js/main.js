5ea6a791f041a2d0fd62b36785a1299023966288
ost0102 <62882954+ost0102@users.noreply.github.com>
2025-10-28 15:41:54 +0900

    메인

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
 // GSAP 플러그인 로드
 gsap.registerPlugin(ScrollTrigger);
 
-// const imgHalf = 270; // 이미지 높이 절반 (540 / 2)
-// const imgTop = window.innerHeight / 2 - imgHalf;
-// const imgBottom = window.innerHeight / 2 + imgHalf;
-// const imgBottom2 = window.innerHeight / 2 + 270; // 50vh + 270px
+const imgHalf = 270; // 이미지 높이 절반 (540 / 2)
+const imgTop = window.innerHeight / 2 - imgHalf;
+const imgBottom = window.innerHeight / 2 + imgHalf;
+const imgBottom2 = window.innerHeight / 2 + 270; // 50vh + 270px
 
-// gsap.to(".img-wrap.wrap1",{
-//     clipPath:"inset(0% 0% 100% 0%)",
-//     scrollTrigger:{
-//         trigger:".s3-box2",
-//         start: `top ${imgBottom}`,
-//         end:`top top`,
-//         scrub:true,
-//         ease:"none",
-//     }
-// })
+gsap.to(".img-wrap.wrap1",{
+    clipPath:"inset(0% 0% 100% 0%)",
+    scrollTrigger:{
+        trigger:".s3-box2",
+        start: `top ${imgBottom}`,
+        end:`top top`,
+        scrub:true,
+        ease:"none",
+    }
+})
 
 
 const slideWrap = document.querySelector('.s5-cont--wrap');
