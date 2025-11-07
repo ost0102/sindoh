a0a4caf9816011c73a1a956da5bdaff164279cd7
ost0102 <62882954+ost0102@users.noreply.github.com>
2025-11-07 14:13:44 +0900

    영상 불러오기 스크립트

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,29 @@
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
 };
+
+document.addEventListener("DOMContentLoaded", () => {
+    const lenis = new Lenis({
+        duration: 1.2,
+        smooth: true,
+    });
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+
+    const videos = document.querySelectorAll("video");
+    if (videos.length > 0) {
+        lenis.stop();
+        Promise.all(
+            Array.from(videos).map(
+                v => new Promise(resolve => v.addEventListener('loadeddata', resolve, { once: true }))
+            )
+        ).then(() => {
+            console.log('모든 비디오 로드 완료');
+            lenis.start();
+        });
+    }
+});
